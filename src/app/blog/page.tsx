8ba22115f720c56456f1aaf0895b646a0279a9c8
mugisham37 <mugisham333@gmail.com>
2026-02-24 18:37:35 +0200

    feat(blog): Add blog page with header and listing components
    - Add blog page template with Webflow HTML export
    - Create blog page route in Next.js app directory
    - Implement blog component with header and content sections
    - Add blog header component for page title and description
    - Migrate Webflow blog template to React JSX components

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '@/components/blog/header'
+import Blog from '@/components/blog/blog'
+import Footer from '@/components/footer'
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <Blog />
+      <Footer />
+    </>
+  )
+}
+
+export default page
